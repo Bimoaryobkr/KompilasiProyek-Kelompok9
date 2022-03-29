05a11479010fe6bdafce986978d64a7906f6cabb
DESKTOP-GCLP4JT\LENOVO <bimoaryob15@gmail.com>
2022-03-29 23:08:04 +0700

    Lexer dulu

---
+++ lexercoba.py
@@ -0,0 +1,35 @@
+from sly import Lexer
+
+class UntukLexer(Lexer):
+
+
+    tokens = {NAME, NUMBER, STRING, PRINT, IF, THEN, ELSE, FOR, TO}
+    ignore = '\t '
+    literals = { '=', '+', '-', '/', 
+                '*', '(', ')', ',', ';'}
+  
+  
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+    PRINT = r'tulis'
+    IF = r'bila'
+    THEN = r'maka'
+    ELSE = r'lain'
+    FOR = r'untuk'
+    TO = r'hingga'
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        
+        t.value = int(t.value) 
+        return t
+  
+    @_(r'//.*')
+    def COMMENT(self, t):
+        pass
+  
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
+
+
