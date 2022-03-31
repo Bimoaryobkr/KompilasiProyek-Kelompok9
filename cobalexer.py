4ce70b72ae9d82dba9a4cccc1321e54b8df6bf3f
DESKTOP-GCLP4JT\LENOVO <bimoaryob15@gmail.com>
2022-03-31 16:49:15 +0700

    Lexer dan Parser (SLY)

---
+++ cobalexer.py
@@ -0,0 +1,48 @@
+from sly import Lexer
+
+class UntukLexer(Lexer):
+
+    tokens = {NAME, NUMBER, STRING, PRINT, IF, THEN, ELSE, FOR, TO, FUN, EQEQ, ARROW}
+    ignore = '\t '
+    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';'}
+  
+    PRINT = r'TULIS'
+    IF = r'BILA'
+    THEN = r'MAKA'
+    ELSE = r'LAIN'
+    FOR = r'UNTUK'
+    TO = r'HINGGA'
+    FUN = r'FUNGSI'
+    ARROW = r'->'
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+
+    EQEQ = r'=='
+    
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        
+        t.value = int(t.value) 
+        return t
+  
+    @_(r'//.*')
+    def COMMENT(self, t):
+        pass
+  
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
+
+if __name__ == '__main__':
+    lexer = UntukLexer()
+    env = {}
+    while True:
+        try:
+            text = input('Cek Lexer > ')
+        except EOFError:
+            break
+        if text:
+            lex = lexer.tokenize(text)
+            for token in lex:
+                print(token)
