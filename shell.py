50857ee593306023467eb73f60572f98bca171db
DESKTOP-GCLP4JT\LENOVO <bimoaryob15@gmail.com>
2022-04-21 15:21:12 +0700

    0

--- shell.py
+++
@@ -1,9 +0,0 @@
-from distutils.log import error
-import kelompok9
-
-while True:
-    text=input('Kelompok9 > ')
-    result, error=kelompok9.Run(text)
-
-    if error: print(error.as_string())
-    else: print(result)
